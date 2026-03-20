abe3a4f2256f4831490cbb1840f55c8052153d2f
Lalitha <lalithab010907@gmail.com>
2026-03-20 16:03:12 +0530

    Time: 6 ms (8.09%) | Memory: 11.2 MB (20.20%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        int n=nums.size();
+        int psize=(1<<n);
+        vector<vector<int>> output;
+        vector<vector<int>> output_final;
+        for(int i=0;i<psize;i++){
+            vector<int> temp;
+            for(int j=0;j<n;j++){
+                if((i&(1<<j))!=0) temp.push_back(nums[j]);
+            }
+            sort(temp.begin(),temp.end());
+            output.push_back(temp);
+        }
+        sort(output.begin(),output.end());
+        output_final.push_back(output[0]);
+        for(int i=1;i<output.size();i++){
+            if(output[i]!=output[i-1])output_final.push_back(output[i]);
+        }
+        return output_final;
+    }
+};
