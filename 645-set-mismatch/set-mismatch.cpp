fc2e270b4aba45f16e2ec363fcfe8a006efb55f6
Lalitha <lalithab010907@gmail.com>
2026-05-12 10:23:16 +0530

    Time: 0 ms (100.00%) | Memory: 25 MB (88.19%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int n=nums.size();
-       vector<int> a(n);
-       for(int i=0;i<n;i++){
-        a[nums[i]-1]+=1;
-       }
-       vector<int> ans;
-       for(int i=0;i<n;i++){
-        if(a[i]==2){
-            ans.push_back(i+1);
+        int n = nums.size();
+        int x = nums[0];
+        int y = 1;
+        for (int i = 1; i < n; i++) {
+            x = x ^ nums[i];
+            y = y ^ (i + 1);
         }
-       }
-       for(int i=0;i<n;i++){
-        if(a[i]==0){
-            ans.push_back(i+1);
+        int z = x ^ y;
+        int k = z & (~(z - 1));
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < n; i++) {
+            if ((nums[i] & k) != 0)
+                a = a ^ nums[i];
+            else
+                b = b ^ nums[i];
         }
-       }
-       return ans; 
+        for (int i = 1; i <= n; i++) {
+            if ((i & k) != 0)
+                a = a ^ i;
+            else
+                b = b ^ i;
+        }
+        for (int i = 0; i < n; i++) {
+            if (a == nums[i])
+                return {a, b};
+        }
+        return {b, a};
     }
 };
